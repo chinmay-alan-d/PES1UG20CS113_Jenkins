d2e5e9f183f195c232b55ac8bfd9e087f028a229
Chinmay Aland <107400674+chinmay-alan-d@users.noreply.github.com>
2023-02-17 14:58:33 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "My SRN is pes1ug20cs113";
+  cout << "My SRN is pes1ug20cs113\n";
   return 0;
 }
